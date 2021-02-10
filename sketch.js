ca8c09dd9020756553d26a12481e34278e94a94d
BimalKesavan <75312466+BimalKesavan@users.noreply.github.com>
2021-02-10 16:50:11 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas;
+var gameState=0;
+var contestantCount,database,quiz,question;
+var contestant;
+var database;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
